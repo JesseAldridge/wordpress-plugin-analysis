da46a7f3a30119dd34fef7c011910a20d935a9d4
Jesse Aldridge <jessealdridge@gmail.com>
2019-03-27 17:45:31 -0700

    elementor reviews to csv

--- _1_scrape_plugins_list.py
+++ _1_scrape_plugins_list.py
@@ -1,4 +1,4 @@
-import re, time, os, shutil, HTMLParser
+import re, time, os, shutil, csv
 
 import requests
 
@@ -16,49 +16,56 @@ def cached_pull(url, secs_sleep_after_request=None):
   resp = requests.get(url)
   if secs_sleep_after_request:
     time.sleep(secs_sleep_after_request)
+  out_str = resp.content.decode('utf-8')
   with open(cache_file_path, 'w') as f:
-    f.write(resp.content)
-  return resp.content
+    f.write(out_str)
+  return out_str
 
 '''
 <h2 class="entry-title"><a href="https://wordpress.org/plugins/contact-form-7/" rel="bookmark">Contact Form 7</a></h2>
 '''
 
 class Writer:
-  def __init__(self, filename, field_names):
+  def __init__(self, filename, column_labels):
     self.filename = filename
     if os.path.exists(filename):
       shutil.move(filename, filename + '.old')
+    self.column_labels = column_labels
 
-    with open(filename, 'w') as f:
-      f.write('{}\n'.format(','.join(field_names)))
+  def __enter__(self):
+    self.f = open(self.filename, 'w')
+    self.writer = csv.writer(self.f, lineterminator='\n')
+    self.writer.writerow(self.column_labels)
+    return self
+
+  def __exit__(self, type, value, traceback):
+    self.f.close()
 
   def write(self, vals):
-    with open(self.filename, 'a') as f:
-      f.write('{}\n'.format(','.join([str(x) for x in vals])))
+    self.writer.writerow(vals)
 
 def main():
   url = 'https://wordpress.org/plugins/browse/popular/page/{}/'
   num_pulls = 50
   filename = 'plugins1.csv'
 
-  writer = Writer(filename, ['slug', 'active_installs'])
-  for i in range(1, num_pulls):
-    print 'pulling page:', i
-    html = cached_pull(url.format(i), secs_sleep_after_request=2)
+  with Writer(filename, ['slug', 'active_installs']) as writer:
+    for i in range(1, num_pulls):
+      print('pulling page:', i)
+      html = cached_pull(url.format(i), secs_sleep_after_request=2)
 
-    plugin_links = []
-    link_regex = '<h2 class="entry-title"><a href="https://wordpress.org/plugins/(.+?)/"'
-    active_installs_regex = r'([0-9,a-zA-Z \+]+?) active installations\s+</span>'
-    for link_match, active_installs_match in zip(
-      re.finditer(link_regex, html),
-      re.finditer(active_installs_regex, html),
-    ):
-      slug = link_match.group(1)
-      active_installs_str = re.sub(r'\+ million', '000000', active_installs_match.group(1))
-      active_installs_str = re.sub(',', '', active_installs_str)
-      active_installs_str = re.sub(r'\+', '', active_installs_str)
-      writer.write([slug, int(active_installs_str)])
+      plugin_links = []
+      link_regex = '<h2 class="entry-title"><a href="https://wordpress.org/plugins/(.+?)/"'
+      active_installs_regex = r'([0-9,a-zA-Z \+]+?) active installations\s+</span>'
+      for link_match, active_installs_match in zip(
+        re.finditer(link_regex, html),
+        re.finditer(active_installs_regex, html),
+      ):
+        slug = link_match.group(1)
+        active_installs_str = re.sub(r'\+ million', '000000', active_installs_match.group(1))
+        active_installs_str = re.sub(',', '', active_installs_str)
+        active_installs_str = re.sub(r'\+', '', active_installs_str)
+        writer.write([slug, int(active_installs_str)])
 
 if __name__ == '__main__':
   main()
