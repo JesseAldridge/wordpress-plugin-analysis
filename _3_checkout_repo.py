7983b7b6d4b0eeca7d8726f50cb4c80046c0911a
Jesse Aldridge <jessealdridge@gmail.com>
2019-08-03 13:28:34 -0700

    checkout repos

---
+++ _3_checkout_repo.py
@@ -0,0 +1,42 @@
+import subprocess, os, math
+
+import _2_scrape_plugin_pages
+
+def main():
+  new_labels = [
+    'title', 'slug', 'my_score', 'weighted', 'active_installs', 'weighted', 'relevance',
+    'weighted', 'review_count', 'weighted', 'avg_rating', 'lines_of_code',
+  ]
+  _2_scrape_plugin_pages.augment_csv(pull_svn, new_labels)
+
+
+def pull_svn(row):
+  plugin_slug = row[1]
+  repos_dir = os.path.expanduser('~/wordpress-plugin-repos')
+  if not os.path.exists(repos_dir):
+    os.mkdir(repos_dir)
+  repo_dir = os.path.join(repos_dir, plugin_slug)
+  command_svn = [
+    'svn',
+    'checkout',
+    'https://plugins.svn.wordpress.org/{}/trunk'.format(plugin_slug),
+    repo_dir,
+  ]
+  subprocess.Popen(command_svn).communicate()
+
+  std_out_str = subprocess.Popen(['tokei', repo_dir], stdout=subprocess.PIPE).communicate()[0]
+  lines_of_code = None
+  for line in str(std_out_str, 'utf8').splitlines():
+    print('line:', line)
+    if line.startswith(' Total   '):
+      lines_of_code = int(line.strip().split()[3])
+
+  new_row = [x for x in row] + [lines_of_code]
+  # go for low lines of code counts
+  print('old score:', new_row[2])
+  new_row[2] = (float(new_row[2]) + -math.log(lines_of_code)) / 2
+  print('new score:', new_row[2])
+  return new_row
+
+if __name__ == '__main__':
+  main()
